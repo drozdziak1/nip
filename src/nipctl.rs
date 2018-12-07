0289ae3999ce5697979d1bb1088c9f7a1f09374a
Stan Drozd <drozdziak1@gmail.com>
2018-12-07 10:14:22 +0100

    Add object-listing to nipctl

--- src/nipctl.rs
+++ src/nipctl.rs
@@ -16,7 +16,7 @@ use log::LevelFilter;
 
 use std::process;
 
-use nip_core::{util, NIPIndex, NIPRemote};
+use nip_core::{util, NIPIndex, NIPObject, NIPRemote};
 
 pub fn main() {
     util::init_logging(LevelFilter::Info);
@@ -50,12 +50,33 @@ pub fn main() {
                     process::exit(1);
                 });
 
-            debug!("Fetching index at {}", nip_remote.to_string());
+            debug!("Parsed link {}", nip_remote.to_string());
 
-            let idx = NIPIndex::from_nip_remote(&nip_remote, &mut ipfs).unwrap();
-
-            println!("{:#?}", idx);
+            match NIPIndex::from_nip_remote(&nip_remote, &mut ipfs) {
+                Ok(idx) => {
+                    println!("nip index at {}:\n{:#?}", nip_remote.to_string(), idx);
+                }
+                Err(e) => {
+                    debug!(
+                        "Identifying {} as an index failed: {}",
+                        nip_remote.to_string(),
+                        e
+                    );
+                    match NIPObject::ipfs_get(&nip_remote.to_string(), &mut ipfs) {
+                        Ok(obj) => {
+                            println!("nip object at {}:\n{:#?}", nip_remote.to_string(), obj);
+                        }
+                        Err(e) => {
+                            error!(
+                                "Could not list nip index/object at {}: {}",
+                                nip_remote.to_string(), e
+                            );
+                            process::exit(1);
+                        }
+                    }
+                }
+            }
         }
-        _ => {}
+        _ => unimplemented!(),
     }
 }
