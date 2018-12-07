269886e1de129776249638cf2adf7a71caf1ff59
Stan Drozd <drozdziak1@gmail.com>
2018-12-07 16:47:58 +0100

    nipctl: Implement index rollbacks

--- src/nipctl.rs
+++ src/nipctl.rs
@@ -14,7 +14,7 @@ use failure::Error;
 use ipfs_api::IpfsClient;
 use log::LevelFilter;
 
-use std::process;
+use std::{process, str::FromStr};
 
 use nip_core::{util, NIPIndex, NIPObject, NIPRemote};
 
@@ -26,12 +26,19 @@ pub fn main() {
         .about("The repo administration utility for nip.")
         .subcommand(
             SubCommand::with_name("list")
-                .about("Prints out a nip index human-readably")
+                .about("Prints out a nip IPFS/IPNS link of any type human-readably")
                 .arg(
                     Arg::with_name("ipfs_hash")
                         .help("The IPFS/IPNS hash to get the index from")
                         .required(true)
                         .index(1),
+                )
+                .arg(
+                    Arg::with_name("rollback")
+                        .short("r")
+                        .long("--rollback")
+                        .value_name("N")
+                        .help("When listing an index move at most N steps back using the previous IPFS hash index field; Ignored for objects"),
                 ),
         )
         .get_matches();
@@ -54,7 +61,52 @@ pub fn main() {
 
             match NIPIndex::from_nip_remote(&nip_remote, &mut ipfs) {
                 Ok(idx) => {
-                    println!("nip index at {}:\n{:#?}", nip_remote.to_string(), idx);
+                    let rollback_count: u32 = matches
+                        .value_of("rollback")
+                        .map_or_else(|| Ok(0), |val| val.parse())
+                        .unwrap_or_else(|e| {
+                            error!("Could not parse rollback count: {}", e);
+                            process::exit(1);
+                        });
+
+                    debug!("Requested {} rollback(s).", rollback_count);
+
+                    let mut idx = idx;
+                    let mut current_remote = nip_remote;
+                    let mut i = 0;
+                    while let Some(prev_idx_hash) = idx.prev_idx_hash.clone() {
+                        if i >= rollback_count {
+                            break;
+                        }
+
+                        let new_remote = NIPRemote::from_str(&prev_idx_hash).unwrap_or_else(|e| {
+                            error!("Could not parse previous hash {}: {}", prev_idx_hash, e);
+                            process::exit(1)
+                        });
+
+                        info!(
+                            "Reverting {} => {}",
+                            current_remote.to_string(),
+                            new_remote.to_string()
+                        );
+
+                        current_remote = new_remote;
+
+                        idx = NIPIndex::from_nip_remote(&current_remote, &mut ipfs).unwrap_or_else(
+                            |e| {
+                                error!("Could not get index {} from IPFS: {}", prev_idx_hash, e);
+                                process::exit(1);
+                            },
+                        );
+
+                        i += 1;
+                    }
+
+                    if i < rollback_count {
+                        warn!("Only {} rollbacks were made ({} requested, current index chain ends at {})", i, rollback_count, current_remote.to_string());
+                    }
+                    info!("nip index at {}:", current_remote.to_string());
+                    println!("{:#?}", idx);
                 }
                 Err(e) => {
                     debug!(
@@ -62,14 +114,17 @@ pub fn main() {
                         nip_remote.to_string(),
                         e
                     );
+
                     match NIPObject::ipfs_get(&nip_remote.to_string(), &mut ipfs) {
                         Ok(obj) => {
-                            println!("nip object at {}:\n{:#?}", nip_remote.to_string(), obj);
+                            info!("nip object at {}:", nip_remote.to_string());
+                            println!("{:#?}", obj);
                         }
                         Err(e) => {
                             error!(
                                 "Could not list nip index/object at {}: {}",
-                                nip_remote.to_string(), e
+                                nip_remote.to_string(),
+                                e
                             );
                             process::exit(1);
                         }
