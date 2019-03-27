00d0e0f81266d246a8b11e07fb261c7573e91ba1
Stan Drozd <drozdziak1@gmail.com>
2019-03-27 14:34:06 +0100

    dev_bootstrap.sh: Print the exact locations

--- dev_bootstrap.sh
+++ dev_bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 build_type=${1:-debug}
+
+set -x
 ln -sf $PWD/target/$build_type/git-remote-nip $HOME/.cargo/bin/git-remote-nipdev
 ln -sf $PWD/target/$build_type/nipctl $HOME/.cargo/bin/nipdevctl
